abc111badbae44cb3e26a9e6a5b387178727bea1
6431503083 <6431503083@lamduan.mfu.ac.th>
2024-03-31 00:59:54 +0700

    Success check&& update Controller(edit)

--- Controllers/edit.js
+++ Controllers/edit.js
@@ -1,6 +1,8 @@
 exports.list = async (req, res) => {
   try {
-    res.render("edit");
+    req.session.user
+      ? res.render("edit", { userLoggedIn: true, user: req.session.user })
+      : res.render("edit", { userLoggedIn: false });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
