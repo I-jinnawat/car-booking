98dcf732ccb4cb0417d2fac425d89bc78827f346
Piyapat Inphook <6431503095@lamduan.mfu.ac.th>
2024-04-04 16:43:42 +0700

    update final

--- Public/JS/calendar.js
+++ Public/JS/calendar.js
@@ -26,3 +26,30 @@ document.addEventListener("DOMContentLoaded", function () {
     fullCalendar.render();
   }
 });
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    navLinks: true, // can click day/week names to navigate views
+    // editable: true,
+    selectable: true,
+    events: '/events', // Fetch events from backend on load
+    eventClick: function (info) {
+      // Handle event click if needed
+      console.log(info);
+    }
+  });
+  calendar.render();
+
+  // Initialize Flatpickr for date inputs
+  flatpickr("#eventStart, #eventEnd", {
+    enableTime: true,
+    dateFormat: "Y-m-dTH:i",
+  });
+
+
+});
