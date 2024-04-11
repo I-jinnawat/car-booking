25944b5da84d14e17207e2f0e259e6e0ed58a045
6431503083 <6431503083@lamduan.mfu.ac.th>
2024-04-11 13:41:38 +0700

    Update

--- Models/vehicles.js
+++ Models/vehicles.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const CarSchema = new mongoose.Schema(
   {
-    register: { type: String, required: true, unique: true },
-    type: { type: String, required: true },
-    color: { type: String, require: true },
-    seat: { type: Number, require: true },
-    available: { type: Boolean, require: true },
+    register: {type: String, required: true, unique: true},
+    type: {type: String, required: true},
+    seat: {type: Number, require: true},
+    available: {type: Boolean, require: true, default: true},
   },
-  { timestamps: true }
+  {timestamps: true}
 );
 
-const vehicles = mongoose.model("vehicles", CarSchema);
+const vehicles = mongoose.model('vehicles', CarSchema);
 
 module.exports = vehicles;
