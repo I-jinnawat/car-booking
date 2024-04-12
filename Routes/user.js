76a352c0cfb8e3a8d50392085a4daa9f48e546eb
6431503083 <6431503083@lamduan.mfu.ac.th>
2024-04-12 13:50:24 +0700

    Member can update and deleted

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { list, update, create, read } = require("../Controllers/user");
+const {list, update, create, read} = require('../Controllers/user');
 
-router.post("/user", create);
-router.get("/user", list);
-router.get("/user/:id", read);
-router.post("/user/:id", update);
+router.post('/user', create);
+router.get('/user', list);
+router.get('/user/:id', read);
+router.post('/user/:id', update);
 // router.delete("/user:id", remove);
 module.exports = router;
