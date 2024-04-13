028b55c661ec3652d8b172cf6ae78b00a78082cb
6431503083 <6431503083@lamduan.mfu.ac.th>
2024-04-13 21:03:37 +0700

    Can change password

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcryptjs");
-const session = require("express-session");
-const Auth = require("../Models/Auth");
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const Auth = require('../Models/Auth');
 
 exports.create = async (req, res) => {
   const {
@@ -23,11 +23,10 @@ exports.create = async (req, res) => {
       organization,
       role,
     });
-    // res.redirect("/");
-    res.send("sucessfully");
+
+    res.send('sucessfully');
   } catch (err) {
     console.error(err);
-    // res.redirect("/register");
   }
 };
 
@@ -38,43 +37,50 @@ exports.list = async (req, res) => {
     // res.send("Successfully");
   } catch (error) {
     console.log(error);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 };
+
 exports.update = async (req, res) => {
-  const { id } = req.params;
-  const { newfirstname, newlastname, password } = req.body;
+  const {id} = req.params;
+  const {newfirstname, newlastname, oldpassword, newpassword} = req.body; // Added oldpassword and newpassword
 
   try {
     let user = await Auth.findById(id); // Finding the user by id
     if (!user) {
       // If user not found
-      return res.status(404).json({ msg: "User not found" });
+      return res.status(404).send('user not found');
     }
     // Updating user's first name and last name
-    user.firstname = newfirstname;
-    user.lastname = newlastname;
-    // If password provided, update password
-    if (password) {
-      user.password = bcrypt.hashSync(password, 10);
+    if (newfirstname && newlastname) {
+      user.firstname = newfirstname;
+      user.lastname = newlastname;
+    }
+    // If old password and new password provided, verify old password and update password
+    if (oldpassword && newpassword) {
+      if (!bcrypt.compareSync(oldpassword, user.password)) {
+        // If old password does not match
+        return res.status(400).send('password wrong');
+      }
+      user.password = bcrypt.hashSync(newpassword, 10);
     }
     // Saving the updated user
     await user.save();
 
     // Update session user info if needed (not implemented here)
 
-    res.redirect("/profile/" + id); // Redirect to the user's profile page after successful update
+    res.redirect('/profile/' + id); // Redirect to the user's profile page after successful update
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 };
 
 exports.read = async (req, res) => {
   try {
     const id = req.params.id;
-    const User = await Auth.findOne({ _id: id }).exec();
-    res.send(User);
+    const User = await Auth.findOne({_id: id}).exec();
+    // res.send(User);
   } catch (error) {
     console.log(error);
   }
--- Routes/user.js
+++ Routes/user.js
@@ -6,6 +6,6 @@ const {list, update, create, read} = require('../Controllers/user');
 router.post('/user', create);
 router.get('/user', list);
 router.get('/user/:id', read);
-router.post('/user/:id', update);
+router.post('/user/update/:id', update);
 // router.delete("/user:id", remove);
 module.exports = router;
