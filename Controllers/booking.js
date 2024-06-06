a47a94386a5051520908559a6285b91f09aae90b
I-jinnawat <jinnawatinyim@gmail.com>
2024-06-06 22:11:15 +0700

    แก้ไขรหัสการจอง

--- Controllers/booking.js
+++ Controllers/booking.js
@@ -37,29 +37,23 @@ exports.bookingEdit = async (req, res) => {
     const booking = await Booking.findById(id);
     const drivers = await User.find({role: 'driver'});
     const vehicle = await Vehicle.find({available: 'available'});
+
     if (!booking) {
-      return (
-        res
-          .status(404)
-          // .json({error: 'Booking not found'})
-          .redirect('/manage')
-      );
+      return res.status(404).redirect('/manage');
     }
 
-    // Pass booking status to the frontend
     const bookingStatus = booking.status;
     const errorBooking = req.flash('errorBooking');
-    req.session.user
-      ? res.render('booking-edit', {
-          userLoggedIn: true,
-          user: req.session.user,
-          booking,
-          bookingStatus,
-          drivers,
-          vehicle,
-          errorBooking: errorBooking,
-        })
-      : res.render('dashboard', {userLoggedIn: false});
+
+    res.render('booking-edit', {
+      userLoggedIn: !!req.session.user,
+      user: req.session.user,
+      booking,
+      bookingStatus,
+      drivers,
+      vehicle,
+      errorBooking,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({error: 'Internal Server Error'});
@@ -70,23 +64,16 @@ exports.createEvent = async (req, res) => {
   try {
     const currentYear = new Date().getFullYear();
 
-    // Get or initialize the counter for the current year
     let counter = await Counter.findOneAndUpdate(
       {year: currentYear},
       {$inc: {count: 1}},
       {new: true, upsert: true}
     );
 
-    // Construct the booking ID
     const bookingID = `${currentYear}-${counter.count}`;
-
-    // Add the booking ID to the request body
     req.body.bookingID = bookingID;
 
-    const status = req.body.status;
-
-    // Create the booking in the database
-    const event = await Booking.create(req.body);
+    await Booking.create(req.body);
 
     res.status(201).redirect('/manage');
   } catch (error) {
@@ -95,34 +82,12 @@ exports.createEvent = async (req, res) => {
   }
 };
 
-exports.Event = async (req, res) => {
-  try {
-    const currentUser = req.session.user;
-
-    let events;
-
-    if (currentUser.role === 'approver' || currentUser.role === 'admin') {
-      // Admin and approvers can see all events with their actual titles
-      events = await Booking.find({status: {$gte: 2, $lt: 4}}).lean();
-    } else {
-      // Regular users can only see events with status >= 2 and < 4, and "booked" as title
-      events = await Booking.find({status: {$gte: 2, $lt: 4}}).lean(); // Ensure status >= 2 and < 4
-      events = events.map(event => ({...event, title: 'ถูกจองแล้ว'}));
-    }
-
-    res.json(events);
-  } catch (error) {
-    console.error('Error fetching events:', error);
-    res.status(500).json({error: error.message});
-  }
-};
-
 exports.updateEvent = async (req, res) => {
   const {id} = req.params;
-  const currentBooking = await Booking.findById(id);
-  const user = req.session.user;
 
   try {
+    const currentBooking = await Booking.findById(id);
+
     const {
       status,
       vehicle,
@@ -146,7 +111,6 @@ exports.updateEvent = async (req, res) => {
       total_kilometer,
       completion_Time,
       deletedPassengerIndex,
-      check,
     } = req.body;
 
     if (
@@ -157,16 +121,17 @@ exports.updateEvent = async (req, res) => {
       passengers.splice(deletedPassengerIndex, 1);
     }
 
-    const existingDate = currentBooking.start;
     const existingBooking = await Booking.findOne({
       status: 2,
-      start: existingDate,
+      start: currentBooking.start,
     });
 
-    if (existingBooking && currentBooking.status === 1) {
-      // Render a page with SweetAlert to ask for confirmation
-      res.render('confirm-approval', {bookingId: id});
-      return;
+    if (
+      existingBooking &&
+      currentBooking.status === 1 &&
+      currentBooking.role === 'approver'
+    ) {
+      return res.render('confirm-approval', {bookingId: id});
     }
 
     await Booking.findByIdAndUpdate(id, {
@@ -200,6 +165,25 @@ exports.updateEvent = async (req, res) => {
   }
 };
 
+exports.Event = async (req, res) => {
+  try {
+    const currentUser = req.session.user;
+    let events;
+
+    if (currentUser.role === 'approver' || currentUser.role === 'admin') {
+      events = await Booking.find({status: {$gte: 2, $lt: 4}}).lean();
+    } else {
+      events = await Booking.find({status: {$gte: 2, $lt: 4}}).lean();
+      events = events.map(event => ({...event, title: 'ถูกจองแล้ว'}));
+    }
+
+    res.json(events);
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
+};
+
 exports.deleteEvent = async (req, res) => {
   const {id} = req.params;
 
@@ -217,19 +201,16 @@ exports.confirmBooking = async (req, res) => {
   const user = req.session.user;
 
   try {
-    // Ensure user is authenticated
     if (!user) {
       return res.status(401).send('Unauthorized');
     }
 
-    // Find the booking to be approved
     const booking = await Booking.findById(id);
 
     if (!booking) {
       return res.status(404).send('Booking not found');
     }
 
-    // Update the booking status to approved
     booking.status = 2;
     booking.approverName = `${user.firstname} ${user.lastname}`;
     booking.approve_Time = new Date().toISOString();
